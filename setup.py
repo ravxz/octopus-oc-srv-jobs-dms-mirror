eb56c601d51b5c799e5c515faba8f209759cf3c0
Konstantin Zaporozhtsev <kzaporozhtsev@gmail.com>
2023-09-01 10:31:07 +0300

    initial

---
+++ setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+from setuptools import setup
+
+__version = "1.4.0"
+
+setup(
+    name="oc-dms-mirror",
+    version=__version,
+    description="Tool for mirroring DMS artifacts",
+    long_description="Tool for mirroring DMS artifacts",
+    long_description_content_type="text/plain",
+    install_requires=[
+        "oc-cdtapi >= 3.10.0",
+        "oc-checksumsq >= 10.0.3"
+    ],
+    packages=["oc_dms_mirror"])
+
