0d819afbd3f48a576d327ec6cc3e4230e9516a1f
Ravil Zakirov <ravilzzakirov@gmail.com>
2024-02-29 21:15:39 +0300

    Added negative test for rest_api.

--- oc_dms_mirror/tests/test_rest_api.py
+++ oc_dms_mirror/tests/test_rest_api.py
@@ -17,7 +17,7 @@ class RestApiTestSuite(unittest.TestCase):
         with app.app_context():
             self.test_client = app.test_client()
 
-    def test_register_component_version_artifact(self):
+    def test_register_component_version_artifact_ok(self):
         with unittest.mock.patch('oc_dms_mirror.rest_api.app.routes.get_dms_mirror') as _get_dms_mirror:
             _dmsMirror = unittest.mock.MagicMock()
             _dmsMirror.process_version = unittest.mock.MagicMock()
@@ -33,5 +33,24 @@ class RestApiTestSuite(unittest.TestCase):
             response = self.test_client.post("/register-component-version-artifact", json=data)
             self.assertEqual(response.status_code, 200)
             self.assertEqual(response.json.get('result'), 'Success')
+
+            _dmsMirror.process_version.assert_called_once_with("my_version", "my_component")
+
+    def test_register_component_version_artifact_fail(self):
+        with unittest.mock.patch('oc_dms_mirror.rest_api.app.routes.get_dms_mirror') as _get_dms_mirror:
+            _dmsMirror = unittest.mock.MagicMock()
+            _dmsMirror.process_version = unittest.mock.MagicMock(side_effect=Exception('processing failed'))
+            _get_dms_mirror.return_value = _dmsMirror
+            self.create_app()
+
+            data = {
+                "type": "register-component-version-artifact",
+                "component": "my_component",
+                "version": "my_version",
+                "artifact": {}
+            }
+            response = self.test_client.post("/register-component-version-artifact", json=data)
+            self.assertEqual(response.status_code, 400)
+            self.assertEqual(response.json.get('result'), 'processing failed')
 
             _dmsMirror.process_version.assert_called_once_with("my_version", "my_component")
